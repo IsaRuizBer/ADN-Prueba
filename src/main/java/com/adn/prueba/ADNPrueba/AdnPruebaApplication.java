dedca3cc4b8b23a95e8e2ad8f45ad4c9478643ce
Isaura Leinar Ruíz Bernal - Ceiba Software <isaura.ruiz@ceiba.com.co>
2020-09-25 12:46:28 -0500

    Ejercicio Gradel, Jenkins y Sonar

---
+++ src/main/java/com/adn/prueba/ADNPrueba/AdnPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.adn.prueba.ADNPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adn/prueba/ADNPrueba/AdnPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adn.prueba.ADNPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
